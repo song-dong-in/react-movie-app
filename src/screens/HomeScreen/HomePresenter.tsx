805cf807fe59e0915748301d5c1e6d9e4b1d0fad
song-dong-in <sdi9954@naver.com>
2023-09-02 11:29:37 +0900

    feat : home title 추가

--- src/screens/HomeScreen/HomePresenter.tsx
+++ src/screens/HomeScreen/HomePresenter.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ProTypes from 'prop-types';
 import Loader from '../../components/Loader/Loader';
 import styles from './Home.module.css';
+import {Helmet, HelmetProvider } from 'react-helmet-async';
 
 
 interface HomeProps{
@@ -19,9 +20,17 @@ const HomPresenter : React.FC<HomeProps> = ({
 }) => {
     return loading?  (
         <Loader></Loader>
-        ) : (
-        <div>홈</div>
-        )
+    ) : (
+        <div className = {styles.container}>
+            <HelmetProvider>
+                    <Helmet>
+                        <title>넷플릭스 - 홈</title>
+                    </Helmet>
+            </HelmetProvider>
+
+            <div>컨텐츠 적을 예정</div>
+        </div>
+    )
 }
 
 HomPresenter.propTypes = {
