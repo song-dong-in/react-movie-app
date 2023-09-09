000230181202a6a296cf09370f33b108c7fd5a58
song-dong-in <sdi9954@naver.com>
2023-09-09 14:33:04 +0900

    feat : detail pages

--- src/screens/DetailScreen/DetailContainer.tsx
+++ src/screens/DetailScreen/DetailContainer.tsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import DetailPresenter from "./DetailPresenter";
 import withRouter from "../../utils/withRouter";
+import { movieApi } from "../../api/movie";
 
 interface DetailContainerstate{
     result : any | null,
@@ -8,14 +9,18 @@ interface DetailContainerstate{
     loading : boolean,
     recommendations : any,
     cast : any,
-    keyword : any,
+    keywords : any,
     backdrops : any,
     posters : any,
     tvDetail2 : any,
     reviews : any
 }
 
-class DetailContainer extends Component<{}, DetailContainerstate>{
+
+
+
+
+class DetailContainer extends Component<{params : number}, DetailContainerstate>{
 
     constructor(props : any){
         super(props);
@@ -25,18 +30,54 @@ class DetailContainer extends Component<{}, DetailContainerstate>{
             loading : true,
             recommendations : [null],
             cast : [],
-            keyword : [],
+            keywords : [],
             reviews : [],
             backdrops : [],
             posters : [],
             tvDetail2 : [],
+        };
+    }
+
+    async componentDidMount(){
+        try{
+            const parsedId = this.props.params;
+            
+            const {data : result} = await movieApi.movieDetail(parsedId);
+            const { data : {results : recommendations}} = await movieApi.recommendations(parsedId);   
+
+            const {data : {cast}} = await movieApi.credits(parsedId);
+
+            const {data : {keywords}} = await movieApi.keywords(parsedId);
+            
+            const {data : {results : reviews}} = await movieApi.reviews(parsedId);
+            const { 
+                data : {backdrops },
+                data : {posters}
+            } = await movieApi.images(parsedId);
+
+            console.log(keywords);
+            this.setState({
+                result,
+                recommendations,
+                cast,
+                keywords,
+                reviews,
+                backdrops : backdrops && backdrops,
+                posters : posters && posters,
+                loading : false,
+                error : null
+            });
+        }
+        catch(err){
+
+            this.setState({error : "상세정보를 가져올 수 없습니다."});
         }
     }
 
 
 
     render(){
-        return <DetailPresenter></DetailPresenter>
+        return <DetailPresenter{...this.state}/>
     }
 }
 
